cbd4673ebcadd7a44c7334917b365e37828d6442
bilzard <nakamine11@gmail.com>
2022-11-08 03:21:53 +0900

    support github

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -7,13 +7,38 @@ const replaceMap = {
         "h1.ltx_title",
         ".ltx_authors",
     ],
+    "github.com": [
+        "header.Header",
+        "div#repository-container-header",
+        "div.file-navigation",
+        "div.Box-header",
+        "nav.menu",
+        "div.mb-3",
+        "table",
+        "h2#blob-path",
+        ".btn",
+    ]
+}
+
+function setNoTranslate(rootElement, patterns) {
+    for (const selector of patterns) {
+        for (const element of rootElement.querySelectorAll(selector)) {
+            element.classList.add("notranslate");
+        }
+    }
 }
 
 const pageHost = window.location.host;
 const patterns = pageHost in replaceMap ? replaceMap[pageHost] : [];
+setNoTranslate(document, patterns);
 
-for (const selector of patterns) {
-    for (const element of document.querySelectorAll(selector)) {
-        element.classList.add("notranslate");
-    }
-}
+const observer = new MutationObserver((mutations, _) => {
+    mutations.forEach((mutation) => {
+        setNoTranslate(mutation.target, patterns);
+    });
+});
+
+observer.observe(document.body,
+    {
+        childList: true, subtree: true, attributes: false, characterData: false
+    });
